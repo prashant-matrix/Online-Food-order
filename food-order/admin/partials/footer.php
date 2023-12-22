d14d2a4d464e217cd3c14b40a3c7dd4df469d487
prashant-matrix <72510700+prashant-matrix@users.noreply.github.com>
2023-12-22 15:28:14 +0545

    Add files via upload

---
+++ food-order/admin/partials/footer.php
@@ -0,0 +1,14 @@
+<!--Footer Section Starts-->
+<div class="footer">
+        <div class="wrapper">
+          <p class="text-center">2020 All rights reserved,Khaja Ghar Developed By- <a href="#"> Rabi and Prashant</a></p>
+            
+        </div> 
+      </div>
+      <!--Footer Section Ends-->  
+ 
+
+
+
+  </body>
+</html>
